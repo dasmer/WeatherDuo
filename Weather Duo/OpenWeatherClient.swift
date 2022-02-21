4845ad7bbb5431635fee2b873ee1fa4493bfcdec
Dasmer Singh <DasmerSingh@gmail.com>
2022-02-21 18:56:13 -0500

    Add project files

---
+++ Weather Duo/OpenWeatherClient.swift
@@ -0,0 +1,6 @@
+import Foundation
+
+private let api = "334ef71349f357adfe939275033cc49a"
+// Current: api.openweathermap.org/data/2.5/weather?lat={lat}&lon={lon}&appid={API key}
+// OneCall https://api.openweathermap.org/data/2.5/onecall?lat={lat}&lon={lon}&appid={API key}
+
---
+++ Weather Duo/Weather_DuoApp.swift
@@ -0,0 +1,17 @@
+//
+//  Weather_DuoApp.swift
+//  Weather Duo
+//
+//  Created by Dasmer Singh on 2/21/22.
+//
+
+import SwiftUI
+
+@main
+struct Weather_DuoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
