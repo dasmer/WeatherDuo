b17b789fef30bc61f9691a0681507b9c7648d919
Dasmer Singh <DasmerSingh@gmail.com>
2022-02-21 19:26:06 -0500

    Create OpenWeatherClient and LocationCoordinate

---
+++ Weather Duo/LocationCoordinate.swift
@@ -0,0 +1,7 @@
+import Foundation
+
+struct LocationCoordinate {
+
+    let latitude: Double
+    let longitude: Double
+}
--- Weather Duo/OpenWeatherClient.swift
+++ Weather Duo/OpenWeatherClient.swift
@@ -1,6 +1,34 @@
 import Foundation
 
-private let api = "334ef71349f357adfe939275033cc49a"
 // Current: api.openweathermap.org/data/2.5/weather?lat={lat}&lon={lon}&appid={API key}
 // OneCall https://api.openweathermap.org/data/2.5/onecall?lat={lat}&lon={lon}&appid={API key}
 
+struct OpenWeatherClient {
+    static fileprivate let apiKey = "334ef71349f357adfe939275033cc49a"
+
+    func loadCurrentWeather(coordinate: LocationCoordinate) async -> CurrentWeatherResponse?  {
+        let url = URL(string: "https://api.openweathermap.org/data/2.5/weather?lat=\(coordinate.latitude)&lon=\(coordinate.longitude)&appid=\(Self.apiKey)")!
+        var response: CurrentWeatherResponse? = nil
+
+        if let (data, _) = try? await URLSession.shared.data(from: url),
+           let decodedResponse = try? JSONDecoder().decode(CurrentWeatherResponse.self, from: data) {
+            response = decodedResponse
+        }
+
+        return response
+    }
+}
+
+struct CurrentWeatherResponse: Codable {
+
+    struct Weather: Codable, Identifiable {
+        var id: String
+        var main: String
+        var description: String
+        var icon: String
+    }
+
+    struct Main: Codable {
+        var temp: Double
+    }
+}
